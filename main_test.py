0227ffe6ae5bb9c4d812802588152599f0d2d553
nelonj <olivianeiljones@gmail.com>
2021-01-27 15:49:37 +0100

    added some more animals for you!

--- main_test.py
+++ main_test.py
@@ -2,3 +2,6 @@ from main import capitaliseString
 
 def test_capitaliseString():
     assert capitaliseString("fish") == "FISH"
+    assert capitaliseString("flamingo") == "FLAMINGO"
+    assert capitaliseString("peacock") == "PEACOCK"
+    assert capitaliseString("sloth") == "SLOTH"
